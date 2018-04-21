5ef754fc3a74774fb4228c6e6f31ee6820de0f2d
luansousa26 <luan2850@gmail.com>
2018-04-21 01:10:45 -0300

    teste do background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: []
+  styleUrls: ['/home.scss']
 })
 export class HomeComponent {
 
