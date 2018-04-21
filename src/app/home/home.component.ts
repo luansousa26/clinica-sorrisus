b7efb99b4bb6bb2b26c6fdb7c52ce6840a3c5c21
luansousa26 <luan2850@gmail.com>
2018-04-21 10:22:18 -0300

    corrigindo o erro do heroku

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['/home.scss']
+  styles: ['']
 })
 export class HomeComponent {
 
