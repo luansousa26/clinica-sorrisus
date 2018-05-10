ed67926ec03c89b08a5aa175b468890f230c066d
Luan Sousa <codeit10@fornecedor.gamasaude.com.br>
2018-05-10 15:34:06 -0300

    Alterado o Login e adicionado o metodo post no java

--- projeto/src/main/java/com/clinicasorrisus/projeto/web/rest/PacienteResource.java
+++ projeto/src/main/java/com/clinicasorrisus/projeto/web/rest/PacienteResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import com.clinicasorrisus.projeto.service.PacienteService;
+import com.codahale.metrics.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,10 +17,28 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 
 @RestController
 @RequestMapping("/api")
 public class PacienteResource {
+	
+	  private final PacienteService pacienteService;
+	  
+	 public PacienteResource(PacienteService pacienteService) {
+		 this.pacienteService = pacienteService;
+	 }
+	 
+	@PostMapping("/salvar-pacientes")
+	@Timed
+	public ResponseEntity<PacienteDTO> criar(@Valid @RequestBody PacienteDTO pacienteDTO) throws URISyntaxException{
+		
+		PacienteDTO savePaciente = pacienteService.save(pacienteDTO);
+		return ResponseEntity.created(new URI("/api/salvar-pacientes/" + savePaciente.getId()))
+	            .headers(HeaderUtil.createEntityCreationAlert("Paciente", savePaciente.getId().toString()))
+	            .body(savePaciente);
+	}
+	
 
 }
