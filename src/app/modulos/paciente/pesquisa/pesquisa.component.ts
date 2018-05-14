48b83c6bbbf598da9a1c7fada2985fac156c302b
Luan Sousa <codeit10@fornecedor.gamasaude.com.br>
2018-05-14 16:49:31 -0300

    alterações

--- src/app/modulos/paciente/pesquisa/pesquisa.component.ts
+++ src/app/modulos/paciente/pesquisa/pesquisa.component.ts
@@ -9,24 +9,25 @@ import { PacienteModel } from '../models/paciente.model';
   styles: []
 })
 export class PesquisaComponent {
-  elementos : PacienteModel[] = [];
-  dataSource : any;
-  constructor(private pesquisaService :PesquisaService){
+  elementos: PacienteModel[] = [];
+  dataSource: any;
+
+  displayedColumns = ['id', 'nome', 'profissao'];
+  constructor(private pesquisaService: PesquisaService) {
      this.getUsuarios();
   }
 
   getUsuarios() {
-    this.pesquisaService.getUsuarios().subscribe((usuarios : PacienteModel[]) => {
+    this.pesquisaService.getUsuarios().subscribe((usuarios: PacienteModel[]) => {
       this.elementos = usuarios;
       this.dataSource = new MatTableDataSource(this.elementos);
-    })
+    });
   }
-  displayedColumns = ['id', 'nome', 'profissao'];
-  
+
 
   applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); 
-    filterValue = filterValue.toLowerCase(); 
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
   }
 }
