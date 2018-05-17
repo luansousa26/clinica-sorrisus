23149741de6d8e2919f0c7a8fce84df137d4267a
Luan Sousa <33549496+luansousa26@users.noreply.github.com>
2018-05-17 11:17:03 -0300

    Inclusão do método de validação de CPF

--- src/app/modulos/paciente/cadastro/cadastro.component.ts
+++ src/app/modulos/paciente/cadastro/cadastro.component.ts
@@ -17,6 +17,7 @@ export class CadastroComponent implements OnInit {
     retorno: any;
     postRetorno: any;
     flagMedicamento: boolean;
+    situacaoCpf: boolean;
     @ViewChild('cadastroForm') cadastroForm : NgForm;
 
     constructor(private cadastroService: CadastroService,public snackBar: MatSnackBar) { }
@@ -26,6 +27,66 @@ export class CadastroComponent implements OnInit {
         this.paciente.endereco = new EnderecoModel();
         this.paciente.dadosClinicos = new DadosClinicosModel();
     }
+  
+  // Valida se o CPF está correto
+   validarCpf(cpf: string) {
+    let soma = 0;
+    let resto;
+
+
+    // deixa apenas os numeros
+    cpf.replace(/[^0-9]+/g, '');
+
+    if (cpf === '') {
+        this.situacaoCpf = false;
+        return false;
+    }
+
+    // Primeiro digito
+    if (cpf === '00000000000' || cpf === '11111111111' || cpf === '22222222222' || cpf == '33333333333' ||
+     cpf === '44444444444' || cpf === '55555555555' ||
+     cpf === '66666666666' || cpf === '77777777777' ||
+     cpf === '88888888888' || cpf === '99999999999') {
+
+        this.situacaoCpf = false;
+        return false;
+
+    }
+    for (let i = 1; i <= 9; i++) {
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        resto = (soma * 10) % 11;
+    }
+
+    if ((resto === 10) || (resto === 11)) {
+        resto = 0;
+
+    }
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+
+        this.situacaoCpf = false;
+        return false;
+    }
+
+    // Segundo digito
+    soma = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+    resto = (soma * 10) % 11;
+
+    if ((resto === 10) || (resto === 11)) {
+        resto = 0;
+        this.situacaoCpf = true;
+    }
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        this.situacaoCpf = false;
+        return false;
+    }
+
+    this.situacaoCpf = true;
+    return true;
+}
 
     salvarDados() {
         this.cadastroService.salvarPaciente(this.paciente).subscribe(resposta => {
