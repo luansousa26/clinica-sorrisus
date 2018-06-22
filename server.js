b038b64fcc5877d85a09b3a75871eb0c14cf4d71
luansousa26 <luan2850@gmail.com>
2018-06-21 23:45:29 -0300

    novo teste

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+  });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
