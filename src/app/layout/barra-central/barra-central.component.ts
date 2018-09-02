fbdc9838ce64f694249d1e08ced12fbfee1dc37f
Luan Sousa <luan26@uninove.edu.br>
2018-09-02 20:22:50 -0300

    Alteração na animação da barra central

--- src/app/layout/barra-central/barra-central.component.ts
+++ src/app/layout/barra-central/barra-central.component.ts
@@ -1,34 +1,38 @@
-import { Component, OnInit,trigger, state, transition, animate, style, keyframes } from '@angular/core';
+import { Component, OnInit, trigger, state, transition, animate, style, keyframes } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-barra-central',
   templateUrl: './barra-central.component.html',
   styleUrls: ['barra-central.scss'],
-  animations:[
-    // coloca a animação apenas onde possuir a diretiva [@menuCentral]="currentState" currentState 
+  animations: [
+    // coloca a animação apenas onde possuir a diretiva [@menuCentral]='currentState' currentState 
     // ativa ou inativa a animação
-    trigger("menuCentral", [
-      state('active', style({})),
-      transition("inactive => active", [
-          animate(1000, keyframes([
-              style({ transform: 'rotate(60deg)' }),
-              style({ transform: 'scale(2)' }),
-              style({ transform: 'scale(1)' }),
-          ]))
+    trigger('menuCentral', [
+      state('inactive', style({
+        marginLeft: '-200%',
+        opacity: '0',
+        transition: '5s'
+      })),
+      transition('inactive => active', [
+        animate(4000, keyframes([
+          style({
+            opacity: '1',
+            marginLeft: '0px'
+          })
+        ]))
       ]),
-      
-  ]),
+    ]),
   ]
 })
 export class BarraCentralComponent implements OnInit {
-  currentState:any;
+  currentState: any;
   constructor(public router: Router) { }
 
   ngOnInit() {
-    this.currentState = "inactive";
+    this.currentState = 'inactive';
     setTimeout(() => {
-        this.currentState = "active";
+      this.currentState = 'active';
     }, 500);
   }
 
