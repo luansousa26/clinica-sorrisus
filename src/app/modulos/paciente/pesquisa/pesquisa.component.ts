3dcc5e65c1234b26608c4a118b6ef639075b1ed7
Luan Sousa <luan26@uninove.edu.br>
2018-09-26 14:15:56 -0300

    alterçaão

--- src/app/modulos/paciente/pesquisa/pesquisa.component.ts
+++ src/app/modulos/paciente/pesquisa/pesquisa.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
-import { PesquisaService } from './pesquisa.service';
-import { PacienteModel } from '../models/paciente.model';
-import { PacienteComponent } from '../paciente.component';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
-import { CadastroService } from '../cadastro/cadastro.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { MatTableDataSource } from "@angular/material";
+import { PesquisaService } from "./pesquisa.service";
+import { PacienteModel } from "../models/paciente.model";
+import { PacienteComponent } from "../paciente.component";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatSnackBar
+} from "@angular/material";
+import { CadastroService } from "../cadastro/cadastro.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-pesquisa',
-  templateUrl: './pesquisa.component.html',
-  styles: ['./../cadastro/cadastro.scss']
+  selector: "app-pesquisa",
+  templateUrl: "./pesquisa.component.html",
+  styles: ["./../cadastro/cadastro.scss"]
 })
 export class PesquisaComponent implements OnInit {
-
   elementos: PacienteModel[] = [];
-  pacienteAtualizacao: PacienteModel;
-  dataSource: any;
   arrayTemp: PacienteModel[] = [];
-  visualizarSideBar: boolean;
-  displayedColumns = ['id', 'nome', 'profissao'];
+  displayedColumns = ["id", "nome", "profissao"];
+
+  pacienteAtualizacao: PacienteModel;
   paciente: PacienteModel = new PacienteModel();
+
+  dataSource: any;
+  visualizarSideBar: boolean;
   situacaoCpf: any;
-  constructor(private pesquisaService: PesquisaService,
-    public snackBar: MatSnackBar) {
+
+  constructor(
+    private pesquisaService: PesquisaService,
+    public snackBar: MatSnackBar
+  ) {
     this.pacienteAtualizacao = new PacienteModel();
   }
   ngOnInit() {
@@ -32,27 +41,30 @@ export class PesquisaComponent implements OnInit {
 
   private getUsuarios(): void {
     this.arrayTemp = [];
-    this.pesquisaService.getUsuarios().subscribe((usuarios: PacienteModel[]) => {
-      this.elementos = usuarios;
+    this.pesquisaService
+      .getUsuarios()
+      .subscribe((usuarios: PacienteModel[]) => {
+        this.elementos = usuarios;
 
-      usuarios.map((elemento) => {
-        const paciente = new PacienteModel();
-        paciente.id = elemento.id;
-        paciente.nomeCompleto = elemento.nomeCompleto;
-        paciente.profissao = elemento.profissao;
-        this.arrayTemp.push(paciente);
+        usuarios.map(elemento => {
+          const paciente = new PacienteModel();
+          paciente.id = elemento.id;
+          paciente.nomeCompleto = elemento.nomeCompleto;
+          paciente.profissao = elemento.profissao;
+          this.arrayTemp.push(paciente);
+        });
+        this.dataSource = new MatTableDataSource(this.arrayTemp);
       });
-      this.dataSource = new MatTableDataSource(this.arrayTemp);
-    });
   }
 
   public linhaSelecionada(linhaSelecionada: PacienteModel): void {
-    this.pacienteAtualizacao = this.elementos.find((paciente) => paciente.id === linhaSelecionada.id);
+    this.pacienteAtualizacao = this.elementos.find(
+      paciente => paciente.id === linhaSelecionada.id
+    );
     this.visualizarSideBar = true;
   }
 
-
- public applyFilter(filterValue: string): void {
+  public applyFilter(filterValue: string): void {
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
@@ -62,4 +74,3 @@ export class PesquisaComponent implements OnInit {
     this.visualizarSideBar = false;
   }
 }
-
--- src/app/servicos/login.service.ts
+++ src/app/servicos/login.service.ts
@@ -1,41 +1,29 @@
-import { LoginModel } from './../login/login.model';
-import {
-  Injectable
-} from '@angular/core';
-import {
-  Http,
-  Response
-} from '@angular/http';
-import 'rxjs/add/operator/map';
-import {
-  HttpClient,
-  HttpResponse
-} from '@angular/common/http';
-import {
-  Observable
-} from 'rxjs/Observable';
-import { Router } from '@angular/router';
+import { LoginModel } from "./../login/login.model";
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import "rxjs/add/operator/map";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class LoginService {
-
   usuarioAutenticado = new LoginModel();
   public logado: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   public usuarioLogado() {
-    return window.sessionStorage.getItem('usuarioLogado');
+    return window.sessionStorage.getItem("usuarioLogado");
   }
-  public logarUsuario(dadosUsuario) {
-    this.logado = 'true';
-    window.sessionStorage.setItem('usuarioLogado', 'true');
+  public logarUsuario(dadosUsuario): void {
+    this.logado = "true";
+    window.sessionStorage.setItem("usuarioLogado", "true");
     this.usuarioAutenticado = dadosUsuario;
-
   }
-  public deslogarUsuario() {
-    this.logado = 'false';
-    window.sessionStorage.setItem('usuarioLogado', 'false');
-    this.router.navigate(['']);
+  public deslogarUsuario(): void {
+    this.logado = "false";
+    window.sessionStorage.setItem("usuarioLogado", "false");
+    this.router.navigate([""]);
   }
 }
